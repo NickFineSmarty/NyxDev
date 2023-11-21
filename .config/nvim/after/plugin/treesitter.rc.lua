6aa25fa9031d087524020cab56fd8fed4f0cf589
NickFineSmarty <140074886+NickFineSmarty@users.noreply.github.com>
2023-11-20 23:41:44 -0800

    nvim
    nvim

---
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -0,0 +1,31 @@
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if (not status) then return end
+
+ts.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "markdown",
+    "markdown_inline",
+    "tsx",
+    "typescript",
+    "toml",
+    "fish",
+    "php",
+    "json",
+    "yaml",
+    "swift",
+    "css",
+    "html",
+    "lua"
+  },
+  autotag = {
+    enable = true,
+  },
+}
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,21 +2,6 @@ require("base")
 require("maps")
 require("color")
 require("plugin")
-require("plugins.neosolarized")
-require("plugins.lualine")
-require("plugins.telescope")
-require("plugins.lsp")
-require("plugins.cmp")
-require("plugins.autopairs")
-require("plugins.colorizer")
-require("plugins.gitsigns")
-require("plugins.bufferline")
-require("plugins.null-ls")
-require("plugins.prettier")
-require("plugins.mason")
-require("plugins.lspsaga")
-require("plugins.treesitter")
-require("plugins.web-devicons")
 
 local has = vim.fn.has
 local is_mac = has "macunix"
@@ -32,7 +17,4 @@ if is_linux == 1 then
 end
 if is_win == 1 then
   require('windows')
-end
-if is_wsl == 1 then
-  require('wsl')
 end
